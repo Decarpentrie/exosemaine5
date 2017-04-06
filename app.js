69d46f9792092d963f9fa14726cf5a0bede4e435
Decarpentrie <elodie.decarpentrie32@gmail.com>
2017-04-06 10:18:03 +0200

    ajout d'une fonction qui génère un tableau de caractères aléatoires

--- app.js
+++ app.js
@@ -1,19 +1,56 @@
- function aleatoire(a) {
+function aleatoire(a) {
  
-    var charactere = ["s", "b", "yop", 5];
+    var charactere = "sbyop";
  
     var chaine = "";
 
     for (i=0; i<a; i++) {
     
+
+
         chaine += charactere[Math.floor(Math.random() * charactere.length)];
     };
-    	console.log(chaine);
     
-    };
-    	aleatoire(10);
+    return chaine;
+   
+   	console.log(chaine);
+};
+
+aleatoire(10);
 
 
 
+function tableau(a, b) {
+
+	var liste = [];
+
+	var result = [];
+
+	for (var i = 0; i <a ; i++) {
+		
+
+
+		for (var j = 0; j < b; j++) {
+			
+
+			liste.push(aleatoire(10));
+
+		}
+		
+		result.push(liste);
+		
+		liste=[];
+
+
+	}
+
+	return result;
+}
+
+var test= tableau(5,5)
+
+console.log(test);
+
+	
 
 
